714e8e6024ab88d440dd0f89efa9d03f346c5525
RafaelOrtiz <rafaeldavid184@gmail.com>
2020-11-09 15:56:14 -0400

    set questions widget conf

--- core/Shortcodes/QuestionsWidget.php
+++ core/Shortcodes/QuestionsWidget.php
@@ -6,16 +6,47 @@ class QuestionsWidget
 {
     public function questions_shortcode($atts = [], $content = null, $tag = '')
     {
-        $atts = keysToLowercase($atts);
+        $output = null;
 
-        // override default attributes with user attributes
+        $atts = $this->keysToLowercase($atts);
+
         $suportedAttributes = [
-            'howgo' => 'default'
+            'message' => 'How did you enjoy this session'
         ];
 
+        // override default attributes with user attributes
         $plugin_atts = shortcode_atts($suportedAttributes, $atts, $tag);
-        
-        return "Dont go " .$plugin_atts['howgo']. " into that good night";
+
+        $message = $plugin_atts['message'];
+
+        if (! is_null($content)) {
+            // secure output by executing the_content filter hook on $content
+            $content = apply_filters('the_content', $content);
+    
+            // run shortcode parser recursively to apply all shortcodes present in the content
+            $content = do_shortcode($content);
+
+            $output .= $content; 
+        }
+
+        $output = <<<EOT
+            <div class="col-12 col-md-3 col-sm-4 mx-auto d-flex align-items-center bg-light" style="border: 1px solid black;">
+                <div class="row bg-white align-self-center justify-content-center">
+                    <span>
+                        <p class="text-center">$message</p>
+                    </span>
+                    <div class="justify-content-center">
+                        <i class="fas fa-star text-warning" style="font-size: xx-large ;"></i>
+                        <i class="fas fa-star text-warning" style="font-size: xx-large ;"></i>
+                        <i class="fas fa-star text-warning" style="font-size: xx-large ;"></i>
+                        <i class="fas fa-star text-warning" style="font-size: xx-large ;"></i>
+                        <i class="fas fa-star text-warning" style="font-size: xx-large ;"></i>
+                    </div>
+                </div>
+            </div>
+EOT;
+
+        return $output;
     }
     
     public function keysToLowercase(array $array) 
